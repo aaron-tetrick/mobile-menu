d385a6e65b037bcd6cbed913d07afe4fb5b357d0
Aaron Tetrick <atetrick14102@gmail.com>
2022-09-24 04:48:58 -0400

    Add media queries and complete design

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-document.addEventListener(e => {
-    
+document.addEventListener('click', e => {
+    const isDropdownBtn = e.target.classList.contains('option');
+    const currentDropdown = e.target.parentElement
+    if (!isDropdownBtn) return
 
+    if (isDropdownBtn) {
+        currentDropdown.classList.toggle('active');
+    }
 
-
+    document.querySelectorAll('.active').forEach(dropdown => {
+        if (dropdown === currentDropdown) return
+        dropdown.classList.remove('active');
+    })
 })
